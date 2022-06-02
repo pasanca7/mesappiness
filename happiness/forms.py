09793cd1cdf5172efb1e19dc4311d3d723302e73
Pablo de los Santos Carrión <pablodelossantoscarrion@gmail.com>
2022-06-03 01:15:28 +0200

    Pagination in Happiness ListView

--- happiness/forms.py
+++ happiness/forms.py
@@ -20,4 +20,15 @@ class HappinessForm(forms.ModelForm):
             'rating': 'Rating',
             'error': 'Error',
         }
+
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs['initial']['user']
+        super(HappinessForm, self).__init__(*args, **kwargs)
+    
+    def save(self, commit=True):
+        obj = super(HappinessForm, self).save(False)
+        obj.user = self.user
+        commit and obj.save()
+        return obj
+
         
--- happiness/views.py
+++ happiness/views.py
@@ -6,7 +6,8 @@ from happiness.models import Happiness
 class ListHappinessView(generic.ListView):
     template_name = 'happiness/list_happiness.html'
     context_object_name = 'happiness_list'
-    pagination_by = 10
+    paginate_by = 5
+    model = Happiness
 
     def get_queryset(self):
         return Happiness.objects.filter(user=self.request.user)
@@ -15,7 +16,11 @@ class ListHappinessView(generic.ListView):
 class CreateHappiness(generic.CreateView):
     template_name = 'happiness/happiness_form.html'
     form_class = HappinessForm
-    success_url = '/account'
+    success_url = '/happiness/list'
+
+    def get_initial(self):
+        self.initial.update({ 'user': self.request.user })
+        return self.initial
 
 
 class ReadHappiness(generic.DetailView):
